6a5bb382a647d87cecd9d55d1d3ebd6ca02a9817
Victor Ruiz <31416135+vicxruiz@users.noreply.github.com>
2023-07-06 03:21:33 -0400

    init

---
+++ ChowNow/App/ChowNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChowNowApp.swift
+//  ChowNow
+//
+//  Created by Victor Ruiz on 7/6/23.
+//
+
+import SwiftUI
+
+@main
+struct ChowNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
