9243ded43af9597342b18563e0cd045f13e1f66a
Victor Ruiz <31416135+vicxruiz@users.noreply.github.com>
2023-07-06 03:48:00 -0400

    add constants for strings and layout

---
+++ ChowNow/Resources/Constants/Constants+Layout.swift
@@ -0,0 +1,16 @@
+//
+//  Constants+Layout.swift
+//  ChowNow
+//
+//  Created by Victor Ruiz on 7/6/23.
+//
+
+import Foundation
+
+struct Layout {
+    static let pd25: CGFloat = 4
+    static let pd50: CGFloat = 8
+    static let pd75: CGFloat = 12
+    static let pd100: CGFloat = 16
+    static let pd125: CGFloat = 20
+}
---
+++ ChowNow/Resources/Constants/Constants+Strings.swift
@@ -0,0 +1,15 @@
+//
+//  Constants+Strings.swift
+//  ChowNow
+//
+//  Created by Victor Ruiz on 7/6/23.
+//
+
+import Foundation
+
+struct Strings {
+    enum Errors {
+        static let tryAgain = "Please try again."
+        static let networkError = "Unable to load data"
+    }
+}
